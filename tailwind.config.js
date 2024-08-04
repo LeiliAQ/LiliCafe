16cad6284eaa0cc1f3d6a06ad371ca5f036f5901
DESKTOP-6E2TF2P\leili <leiliqajar22@gmail.com>
2024-08-04 23:29:44 +1000

    Initialization Activities

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,32 @@
 module.exports = {
   content: ["./public/**/*.{html,js,jsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        "brown":{
+          100:"#ece0d1",
+          300:"#dbc1ac",
+          500:"#967259",
+          900:"#634832",
+        },
+      },
+      boxShadow:{
+        "shadow-custom":"0px 1px 10px rgba(0,0,0,0.05)",
+      },
+      borderRadius:{
+        "radius-custom":"2rem",
+      },
+      fontFamily:{
+        Dana:["Montserrat","sans-serif"],
+        DanaMedium:["MontserratMedium","sans-serif"],
+        DanaDemiBold:["MontserratSemiBold","sans-serif"],
+        MorabbaLight: ['PlayfairLight', 'serif'],
+        MorabbaMedium: ['PlayfairMedium', 'serif'],
+        MorabbaBold: ['PlayfairBold', 'serif'],
+        // Montserrat:['Montserrat'],
+        // Playfair:['Playfair'],
+      }
+    },
   },
   plugins: [],
 }
